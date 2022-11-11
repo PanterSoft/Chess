0eabbd37aff943b0330fbc018d372fce34144323
Nico Mattes <mattesnico@gmail.com>
2022-11-11 11:45:50 +0100

    Refactored TUI Tests

--- src/test/scala/de/htwg/se/Chess/aview/TUI_Test.scala
+++ src/test/scala/de/htwg/se/Chess/aview/TUI_Test.scala
@@ -1,4 +1,8 @@
 package de.htwg.se.Chess.aview
+
+import de.htwg.se.Chess.model.Game
+import de.htwg.se.Chess.controller.Controller
+
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 import javax.swing.text.PlainView
@@ -8,94 +12,80 @@ import scala.io.StdIn
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers
 
-//class TUISpec extends AnyWordSpec:
-//
-//    val TUI = new tui
-//
-//    val expected_field = """   0    1    2    3    4    5    6    7
-///----+----+----+----+----+----+----+----\
-//| R1 | k1 | B1 | Q1 | K1 | B1 | k1 | R1 | 0
-//+----+----+----+----+----+----+----+----+
-//| P1 | P1 | P1 | P1 | P1 | P1 | P1 | P1 | 1
-//+----+----+----+----+----+----+----+----+
-//|    |    |    |    |    |    |    |    | 2
-//+----+----+----+----+----+----+----+----+
-//|    |    |    |    |    |    |    |    | 3
-//+----+----+----+----+----+----+----+----+
-//|    |    |    |    |    |    |    |    | 4
-//+----+----+----+----+----+----+----+----+
-//|    |    |    |    |    |    |    |    | 5
-//+----+----+----+----+----+----+----+----+
-//| P2 | P2 | P2 | P2 | P2 | P2 | P2 | P2 | 6
-//+----+----+----+----+----+----+----+----+
-//| R2 | k2 | B2 | Q2 | K2 | B2 | k2 | R2 | 7
-//\----+----+----+----+----+----+----+----/"""
-//
-//    "commands(start) should create'\n" + expected_field + "'" in {
-//        TUI.commands("start") shouldBe (expected_field)
-//    }
-//
-//    val expected_help_field = """
-//        ------------------------------------
-//        |            HELP TABLE             |
-//        |-----------------------------------|
-//        |   start                           |
-//        |   help              (Display help)|
-//        |   exit             (Close process)|
-//        |                                   |
-//        |   move(x1 y1 x2 y2                |
-//        |   after start current pos x y     |
-//        |   then new pos x y {x y x y}      |
-//        -------------------------------------
-//        """
-//
-//    "commands(help) should create'\n" + expected_help_field + "'" in {
-//        TUI.commands("help") shouldBe (expected_help_field)
-//    }
-//
-//    val expected_welcome_message = """
-//        ------------------------------------
-//        |       Schach - Chess - Game       |
-//        |-----------------------------------|
-//        |      Textbased User Interface     |
-//        |         HTWG Konstanz 2022        |
-//        |              v1.0.0               |
-//        -------------------------------------
-//        """
-//
-//    "welcomeMessage() should create a String'" + expected_welcome_message + "'" //in {
-//        TUI.welcomeMessage shouldBe (expected_welcome_message)
-//    }
-//
-//    "commands(exit) should create a String (Goodbye :))" in {
-//        TUI.commands("exit") shouldBe ("Goodbye :)")
-//    }
-//
-//    val errorMessage = "ERROR! Wrong usage! Try \"help\" !"
-//
-//    "commands() should create a String'" + errorMessage + "'" in {
-//        TUI.commands("") shouldBe (errorMessage)
-//    }
-//
-//    val expected_field_start = """   0    1    2    3    4    5    6    7
-///----+----+----+----+----+----+----+----\
-//| R1 | k1 | B1 | Q1 | K1 | B1 | k1 | R1 | 0
-//+----+----+----+----+----+----+----+----+
-//| P1 | P1 | P1 | P1 | P1 | P1 | P1 | P1 | 1
-//+----+----+----+----+----+----+----+----+
-//|    |    |    |    |    |    |    |    | 2
-//+----+----+----+----+----+----+----+----+
-//|    |    |    |    |    |    |    |    | 3
-//+----+----+----+----+----+----+----+----+
-//|    |    |    |    |    |    |    |    | 4
-//+----+----+----+----+----+----+----+----+
-//|    |    |    |    |    |    |    |    | 5
-//+----+----+----+----+----+----+----+----+
-//| P2 | P2 | P2 | P2 | P2 | P2 | P2 | P2 | 6
-//+----+----+----+----+----+----+----+----+
-//| R2 | k2 | B2 | Q2 | K2 | B2 | k2 | R2 | 7
-//\----+----+----+----+----+----+----+----/"""
-//
-//    "commands() should create a String'" + expected_field_start + "'" in {
-//        TUI.commands("start") shouldBe (expected_field_start)
-//    }
+
+class TUISpec extends AnyWordSpec:
+    val eol = sys.props("line.separator")
+
+    val field = new Game().new_game_map()
+    val controller = new Controller(field)
+    val tui_test = tui(controller)
+
+    val expected_field = "   A    B    C    D    E    F    G    H  " + """
+/----+----+----+----+----+----+----+----\
+| R1 | k1 | B1 | Q1 | K1 | B1 | k1 | R1 | 1
++----+----+----+----+----+----+----+----+
+| P1 | P1 | P1 | P1 | P1 | P1 | P1 | P1 | 2
++----+----+----+----+----+----+----+----+
+|    |    |    |    |    |    |    |    | 3
++----+----+----+----+----+----+----+----+
+|    |    |    |    |    |    |    |    | 4
++----+----+----+----+----+----+----+----+
+|    |    |    |    |    |    |    |    | 5
++----+----+----+----+----+----+----+----+
+|    |    |    |    |    |    |    |    | 6
++----+----+----+----+----+----+----+----+
+| P2 | P2 | P2 | P2 | P2 | P2 | P2 | P2 | 7
++----+----+----+----+----+----+----+----+
+| R2 | k2 | B2 | Q2 | k2 | B2 | k2 | R2 | 8
+\----+----+----+----+----+----+----+----/""" + eol
+
+    "commands(start) should create'\n" + expected_field + "'" in {
+        tui_test.commands("start") shouldBe (expected_field)
+    }
+
+    val expected_help_field = """
+        ------------------------------------
+        |            HELP TABLE             |
+        |-----------------------------------|
+        |   start                           |
+        |   help              (Display help)|
+        |   exit             (Close process)|
+        |                                   |
+        |   move(x1 y1 x2 y2                |
+        |   after start current pos x y     |
+        |   then new pos x y {x y x y}      |
+        -------------------------------------
+        """
+
+    "commands(help) should create'\n" + expected_help_field + "'" in {
+        tui_test.commands("help") shouldBe (expected_help_field)
+    }
+
+    val expected_welcome_message = """
+        ------------------------------------
+        |       Schach - Chess - Game       |
+        |-----------------------------------|
+        |      Textbased User Interface     |
+        |         HTWG Konstanz 2022        |
+        |              v1.0.0               |
+        -------------------------------------
+        """
+
+    "welcomeMessage() should create a String" + expected_welcome_message in {
+        tui_test.welcomeMessage shouldBe (expected_welcome_message)
+    }
+
+    "commands(exit) should create a String (Goodbye :))" in {
+        tui_test.commands("exit") shouldBe ("Goodbye :)")
+    }
+
+    val errorMessage = "ERROR! Wrong usage! Try \"help\" !"
+
+    "commands() should create a String'" + errorMessage + "'" in {
+        tui_test.commands("") shouldBe (errorMessage)
+    }
+
+
+    "commands() should create a String'" + expected_field + "'" in {
+        tui_test.commands("start") shouldBe (expected_field)
+    }
