b650a20db3ac4e3433fb92617cfea41d75483245
stefan.ptacek <stefan.ptacek@uni-konstanz.de>
2022-11-11 11:41:20 +0100

    check move in TUI

--- src/main/scala/de/htwg/se/Chess/Controller/Controller.scala
+++ src/main/scala/de/htwg/se/Chess/Controller/Controller.scala
@@ -5,12 +5,13 @@ import scala.collection.immutable.VectorMap
 import de.htwg.se.Chess.model.Game
 
 val game = new Game().new_game_map()
-
 case class Controller(var map: VectorMap[String, String]) extends Observable:
 
   def board_to_string : String = Game().board_to_string(game)
 
-  def move(posOld : String, posNew : String) : VectorMap[String, String] = Game().move(game,posOld, posNew)
+  def move(posOld : String, posNew : String) : VectorMap[String, String] =
+    notifyObservers
+    Game().move(game,posOld, posNew)
 
   /*
   def addPlayer1(doThis: Move => Field, move: Move) =
