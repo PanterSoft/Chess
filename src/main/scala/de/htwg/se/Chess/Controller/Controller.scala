39c4d040faf2461cd379e8dc289bd7caf50662a8
Nico Mattes <mattesnico@gmail.com>
2022-11-11 10:01:01 +0100

    implemented Game History

--- src/main/scala/de/htwg/se/Chess/Controller/Controller.scala
+++ src/main/scala/de/htwg/se/Chess/Controller/Controller.scala
@@ -1,14 +1,14 @@
 package de.htwg.se.Chess.Controller
 
 import de.htwg.se.Chess.util.Command
-import de.htwg.se.Chess.util.util.Observable
-import de.htwg.se.Chess.util.util.UndoManager
+import de.htwg.se.Chess.util.Observer
+import scala.collection.immutable.VectorMap
+import java.util.Observable
 
-case class Controller(var field: Field) extends Observable:
-  val players = new UndoManager[Field]
+case class Controller(var map: VectorMap[String, String]) extends Observable:
   def addPlayer1(doThis: Move => Field, move: Move) =
-    field = doThis(move)
+    map = doThis(move)
     notifyObservers
   def addPlayer2(doThis: => Field) =
-    field = doThis
+    map = doThis
     notifyObservers
