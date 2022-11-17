64239ce5ec97016bd430a6ac547e7d487c3a09c1
Nico Mattes <mattesnico@gmail.com>
2022-11-17 09:00:05 +0100

    removed Game

--- src/main/scala/de/htwg/se/Chess/model/Game.scala
+++
@@ -1,9 +0,0 @@
-package de.htwg.se.Chess.model
-
-import java.math.MathContext
-import scala.collection.immutable.VectorMap
-import scala.annotation.newMain.apply
-
-class Game(val game_map: VectorMap[String, String]) {
-
-}
